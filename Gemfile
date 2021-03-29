b84264a25ee3e8509027b5f0f9565ae526d7bd77
Nate Montgomery <nathancmontgomery@gmail.com>
2021-03-28 20:42:53 -0700

    Install Spree

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'https://rubygems.org'
+
+gem 'rails', '~> 6.1'
+gem 'spree', '~> 4.2'
+gem 'spree_auth_devise', '~> 4.3'
+gem 'spree_gateway', '~> 3.9'
+gem 'spree_i18n', '~> 5.0'
+
+gem 'pg'
+
+gem 'bootsnap'
+gem 'listen'
---
+++ config/application.rb
@@ -0,0 +1,28 @@
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module AbsurdArchery
+  class Application < Rails::Application
+
+    config.to_prepare do
+      # Load application's model / class decorators
+      Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
+
+      # Load application's view overrides
+      Dir.glob(File.join(File.dirname(__FILE__), "../app/overrides/*.rb")) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
+    end
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.1
+    config.time_zone = "Pacific Time (US & Canada)"
+    config.eager_load_paths << Rails.root.join("lib")
+  end
+end
