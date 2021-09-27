923c02e59ba133642006dbba0fb361ad3a4d6ac9
Nate Montgomery <nathancmontgomery@gmail.com>
2021-09-26 19:40:37 -0700

    Add 404 handling

--- app/controllers/spree/archery_lesson_appointments_controller.rb
+++ app/controllers/spree/archery_lesson_appointments_controller.rb
@@ -3,6 +3,10 @@ module Spree
     include Spree::Core::ControllerHelpers
     include Spree::Core::ControllerHelpers::Store
     include Spree::Core::ControllerHelpers::Currency
+
+    rescue_from ActiveRecord::RecordNotFound, with: :render_404
+
+    helper 'spree/taxons'
     helper Spree::Core::Engine.helpers
 
     def index
@@ -11,18 +15,24 @@ module Spree
       render json: @lesson_appointments
     end
 
-    def create
+    def new
       @product = ArcheryLessonAppointment.new(
         start_time: params[:start_time],
         end_time: params[:end_time],
         product: ::Spree::Product.first
       ).product
 
-      @product_images = @product.images
-      @variants = @product.variants
-      @product_price = @product.price
+      # @product_images = @product.images
+      # @variants = @product.variants
+      # @product_price = @product.price
+
+      @variants = @product.variants_including_master.active(current_currency).includes([:option_values, :images])
+      @product_properties = @product.product_properties.includes(:property)
 
       render :show
+    end
+
+    def create
     end
   end
 end
